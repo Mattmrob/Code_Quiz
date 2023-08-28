91e28d096f3ad3d55d0643a803e92b4fd5d9b9f4
Mysmelf <matthewr9937@gmail.com>
2023-08-28 11:52:04 -0700

    brough total number of quiz questions from 5 to 15

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,14 +32,13 @@ const option2 = document.querySelector("#option2");
 const option3 = document.querySelector("#option3");
 const option4 = document.querySelector("#option4");
 
-let currentQuestion = [[quizStatement], [option1, option2, option3, option4]];
-
 let bool = "";
 let rand = "";
 let questionAnswers = "";
 let correctAnswer = "";
 
 // ----------- QUESTION POOL ----------- 
+// 20 questions to avoid too much repetition, only 10 need to be answered for quiz to end
 
 const question1 = "In coding, a boolean is known as a(n):";
 const answers1 = ["Object","Data-Type","Statement","Element"];
@@ -56,23 +55,39 @@ const answers4 = ["Create a Paragraph Element","Create a P-Icon","Add Parenthesi
 const question5 = "What is the primary function of a For Loop?";
 const answers5 = ["To continuously loop","Loop every time For is mentioned","Refresh the current page","To continuously loop under specified conditions"];
 
-const question6 = "";
-const answers6 = [];
+const question6 = "Which one of these is not a Coding Language?";
+const answers6 = ["HTML", "EMS", "CSS", "JS"];
 
-const question7 = "";
-const answers7 = [];
+const question7 = "What is the purpose of a REAMDE file?";
+const answers7 = ["Provide information on a project", "Provide an intresting narritive story", "Provide text for a webpage", "Provide readable code"];
 
-const question8 = "";
-const answers8 = [];
+const question8 = "What is 'this'?";
+const answers8 = ["That","This","Them","Thus"];
 
-const question9 = "";
-const answers9 = [];
+const question9 = "In HTML, what is a closing tag?";
+const answers9 = ["A statement that closes out the document", "A function that closes the webpage", "An endpoint that contains an element", "The bottom to a text container"];
 
-const question10 = "";
-const answers10 = [];
+const question10 = "What is the purpose of Metadata?";
+const answers10 = ["Provide useful data on the webpage", "Transform the document's style", "Improve browser speed", "Add context to images"];
 
-const questionPool = [question1, question2, question3, question4, question5];
-const answerPool = [answers1, answers2, answers3, answers4, answers5];
+const question11 = "What is a Variable?";
+const answers11 = ["An HTML Element", "A CSS Style", "A JS Function", "A JS Stored Value"];
+
+const question12 = "The Flex display type is used to:";
+const answers12 = ["Stretch an Image","Bend an Element","Create a Flexbox","Form a Flexigon"];
+
+const question13 = "Which of the following is an Array?";
+const answers13 = ["Orangitan", "1, 2, 3, 4", "bigcat + henry", "1234"];
+
+const question14 = "Which of these is a built in JavaScript function commonly used to bugfix?";
+const answers14 = ["console.log()", "bug.fix()", "report.data.type()", "define.error()"];
+
+const question15 = "What is the difference between && and || in JS?";
+const answers15 = ["&& is either, || is both","&& is neither, || is either","&& is either, || is neither","&& is both, || is either"];
+
+const questionPool = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10, question11, question12, question13, question14, question15];
+const answerPool = [answers1, answers2, answers3, answers4, answers5, answers6, answers7, answers8, answers9, answers10, answers11, answers12, answers13, answers14, answers15];
+const optionPool = [option1, option2, option3, option4];
 
 
 // ----------- FUNCTIONS AND EVENTS ----------- 
@@ -106,7 +121,7 @@ function setAllFalse() {
 // Before setting an option to true, all questions are set to false to prevent any carryover from the previous question
 
 function questionSelect() {
-    rand = Math.floor(Math.random() * 5);
+    rand = Math.floor(Math.random() * 15);
     quizStatement.textContent = questionPool[rand];
 
     let questionAnswers = answerPool[rand];
@@ -131,8 +146,37 @@ if (quizStatement.textContent === questionPool[0]) {
 } else if (quizStatement.textContent === questionPool[4]){
     setAllFalse();
     option4.setAttribute("data-selected", "true");
-} else {
-}
+} else if (quizStatement.textContent === questionPool[5]){
+    setAllFalse();
+    option2.setAttribute("data-selected", "true");
+} else if (quizStatement.textContent === questionPool[6]){
+    setAllFalse();
+    option1.setAttribute("data-selected", "true");
+} else if (quizStatement.textContent === questionPool[7]){
+    setAllFalse();
+    option2.setAttribute("data-selected", "true");
+} else if (quizStatement.textContent === questionPool[8]){
+    setAllFalse();
+    option3.setAttribute("data-selected", "true");
+} else if (quizStatement.textContent === questionPool[9]){
+    setAllFalse();
+    option1.setAttribute("data-selected", "true");
+} else if (quizStatement.textContent === questionPool[10]){
+    setAllFalse();
+    option4.setAttribute("data-selected", "true");
+} else if (quizStatement.textContent === questionPool[11]){
+    setAllFalse();
+    option3.setAttribute("data-selected", "true");
+} else if (quizStatement.textContent === questionPool[12]){
+    setAllFalse();
+    option2.setAttribute("data-selected", "true");
+} else if (quizStatement.textContent === questionPool[13]){
+    setAllFalse();
+    option1.setAttribute("data-selected", "true");
+} else if (quizStatement.textContent === questionPool[14]){
+    setAllFalse();
+    option4.setAttribute("data-selected", "true");
+} else {}
 }
 
 // This event listener function targets the button clicked and fetches the data-selected property of the button. If it is "true" (string) then you get a positive message, if "false" (also string) you get a negative message
