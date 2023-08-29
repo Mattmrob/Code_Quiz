cd52ddc6518195842455d2f2cb8f42b59c9416fe
Mysmelf <matthewr9937@gmail.com>
2023-08-29 12:00:33 -0700

    Created submission screen once quiz answers have been answered correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,72 +39,22 @@ const option4 = document.querySelector("#option4");
 
 const timer = document.querySelector("#timer");
 let timeRemaining = 99;
-
 let questionsAnswered = 0;
+
+const submitScore = document.querySelector('#submitScore');
+const yourInitials = document.querySelector('#yourInitials');
+const submissionMenu = document.querySelector('#submissionMenu');
 
 let bool = "";
 let rand = "";
 let questionAnswers = "";
 let correctAnswer = "";
 
-// ----------- QUESTION POOL ----------- 
-// 20 questions to avoid too much repetition, only 10 need to be answered for quiz to end
-
-const question1 = "In coding, a boolean is known as a(n):";
-const answers1 = ["Object","Data-Type","Statement","Element"];
-
-const question2 = "Which one of these values is considered a falsey value?";
-const answers2 = ["1", "Not True", "0", "-1"];
-
-const question3 = "It is considered good practice to link this kind of style sheet before any others in your html document:";
-const answers3 = ["A Bling Sheet", "A Reset Sheet", "A Rap Sheet", "A Base Sheet"];
-
-const question4 = "What does the <p> tag do in an html document?";
-const answers4 = ["Create a Paragraph Element","Create a P-Icon","Add Parenthesis to a Statement","Pair two Elements"];
-
-const question5 = "What is the primary function of a For Loop?";
-const answers5 = ["To continuously loop","Loop every time For is mentioned","Refresh the current page","To continuously loop under specified conditions"];
-
-const question6 = "Which one of these is not a Coding Language?";
-const answers6 = ["HTML", "EMS", "CSS", "JS"];
-
-const question7 = "What is the purpose of a REAMDE file?";
-const answers7 = ["Provide information on a project", "Provide an intresting narritive story", "Provide text for a webpage", "Provide readable code"];
-
-const question8 = "What is 'this'?";
-const answers8 = ["That","This","Them","Thus"];
-
-const question9 = "In HTML, what is a closing tag?";
-const answers9 = ["A statement that closes out the document", "A function that closes the webpage", "An endpoint that contains an element", "The bottom to a text container"];
-
-const question10 = "What is the purpose of Metadata?";
-const answers10 = ["Provide useful data on the webpage", "Transform the document's style", "Improve browser speed", "Add context to images"];
-
-const question11 = "What is a Variable?";
-const answers11 = ["An HTML Element", "A CSS Style", "A JS Function", "A JS Stored Value"];
-
-const question12 = "The Flex display type is used to:";
-const answers12 = ["Stretch an Image","Bend an Element","Create a Flexbox","Form a Flexigon"];
-
-const question13 = "Which of the following is an Array?";
-const answers13 = ["Orangitan", "1, 2, 3, 4", "bigcat + henry", "1234"];
-
-const question14 = "Which of these is a built in JavaScript function commonly used to bugfix?";
-const answers14 = ["console.log()", "bug.fix()", "report.data.type()", "define.error()"];
-
-const question15 = "What is the difference between && and || in JS?";
-const answers15 = ["&& is either, || is both","&& is neither, || is either","&& is either, || is neither","&& is both, || is either"];
-
-const questionPool = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10, question11, question12, question13, question14, question15];
-const answerPool = [answers1, answers2, answers3, answers4, answers5, answers6, answers7, answers8, answers9, answers10, answers11, answers12, answers13, answers14, answers15];
-const optionPool = [option1, option2, option3, option4];
-
-
 // ----------- FUNCTIONS AND EVENTS ----------- 
 
 // Quiz start button - on click it hides the start screen and reveals the quiz content
 
-quizStart.addEventListener("click", function(event){
+beginQuizButton.addEventListener("click", function(event){
     let selectedButton = event.target
     
     if (selectedButton.matches("button") === true) {
@@ -114,17 +64,16 @@ quizStart.addEventListener("click", function(event){
         questionSelect();
         timerStart();
     } else {
-    
     }
     
     })
-
 
 // Quiz Finished function
 
 function quizFinish() {
     quizArea.setAttribute("style", "display:none");
     quizStart.setAttribute("style", "display:flex");
+    submissionMenu.setAttribute("style", "display:flex");
     startEndMessage.textContent = "Yay!";
     startEndDetail.textContent = "This is a test";
     beginQuizButton.textContent = "Go Again?";
@@ -140,11 +89,24 @@ function gameoverScreen() {
     beginQuizButton.textContent = "Try Again?";
 }
 
+// Quiz Score Listener
+submitScore.addEventListener("click", function(event){
+    event.preventDefault();
+    // Run Score Saving Stuff Here
+}) 
+
+yourInitials.addEventListener("submit", function(event){
+    event.preventDefault();
+    // Run Score Saving Stuff Here
+}) 
+
 // Initialize Function - run every time a quiz starts / restarts
 
 function init() {
     timeRemaining = 99;
     questionsAnswered = 0;
+    submissionMenu.setAttribute("style", "display:none");
+    answerResult.textContent = "";
 }
 
 // TIMER FUNCTION
@@ -165,7 +127,6 @@ function timerStart() {
     }
     }, 1000)
 }
-
 
 // ANSWER SELECTION
 
@@ -268,5 +229,57 @@ if (quizStatement.textContent === questionPool[0]) {
     option4.setAttribute("data-selected", "true");
 } else {}
 }
+
+// ----------- QUESTION POOL ----------- 
+// 20 questions to avoid too much repetition, only 10 need to be answered for quiz to end
+
+const question1 = "In coding, a boolean is known as a(n):";
+const answers1 = ["Object","Data-Type","Statement","Element"];
+
+const question2 = "Which one of these values is considered a falsey value?";
+const answers2 = ["1", "Not True", "0", "-1"];
+
+const question3 = "It is considered good practice to link this kind of style sheet before any others in your html document:";
+const answers3 = ["A Bling Sheet", "A Reset Sheet", "A Rap Sheet", "A Base Sheet"];
+
+const question4 = "What does the <p> tag do in an html document?";
+const answers4 = ["Create a Paragraph Element","Create a P-Icon","Add Parenthesis to a Statement","Pair two Elements"];
+
+const question5 = "What is the primary function of a For Loop?";
+const answers5 = ["To continuously loop","Loop every time For is mentioned","Refresh the current page","To continuously loop under specified conditions"];
+
+const question6 = "Which one of these is not a Coding Language?";
+const answers6 = ["HTML", "EMS", "CSS", "JS"];
+
+const question7 = "What is the purpose of a REAMDE file?";
+const answers7 = ["Provide information on a project", "Provide an intresting narritive story", "Provide text for a webpage", "Provide readable code"];
+
+const question8 = "What is 'this'?";
+const answers8 = ["That","This","Them","Thus"];
+
+const question9 = "In HTML, what is a closing tag?";
+const answers9 = ["A statement that closes out the document", "A function that closes the webpage", "An endpoint that contains an element", "The bottom to a text container"];
+
+const question10 = "What is the purpose of Metadata?";
+const answers10 = ["Provide useful data on the webpage", "Transform the document's style", "Improve browser speed", "Add context to images"];
+
+const question11 = "What is a Variable?";
+const answers11 = ["An HTML Element", "A CSS Style", "A JS Function", "A JS Stored Value"];
+
+const question12 = "The Flex display type is used to:";
+const answers12 = ["Stretch an Image","Bend an Element","Create a Flexbox","Form a Flexigon"];
+
+const question13 = "Which of the following is an Array?";
+const answers13 = ["Orangitan", "1, 2, 3, 4", "bigcat + henry", "1234"];
+
+const question14 = "Which of these is a built in JavaScript function commonly used to bugfix?";
+const answers14 = ["console.log()", "bug.fix()", "report.data.type()", "define.error()"];
+
+const question15 = "What is the difference between && and || in JS?";
+const answers15 = ["&& is either, || is both","&& is neither, || is either","&& is either, || is neither","&& is both, || is either"];
+
+const questionPool = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10, question11, question12, question13, question14, question15];
+const answerPool = [answers1, answers2, answers3, answers4, answers5, answers6, answers7, answers8, answers9, answers10, answers11, answers12, answers13, answers14, answers15];
+const optionPool = [option1, option2, option3, option4];
 
 
