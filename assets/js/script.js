5aed91247d017317b8e838531da9b10e2d709f5f
Mysmelf <matthewr9937@gmail.com>
2023-08-29 12:30:01 -0700

    minor visual adjusments to document styles and layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,3 @@
-// Things that need to happen:
-// User pushes button to start quiz, questions begin loading in and timer starts
-// Questions are randomly selected from a pool, randomize button locations as well for bonus points :>
-// User selects a button, if right, next question, if wrong, -15 seconds and next question
-// When quiz is over, user can submit their information to the scoreboard
-// score = time remaining, user info = user's initials (3 chars)
-
-// ORDER TO WORK ON:
-// First step is quiz initialization and question generation
-// Second step is timer functionality
-// Third step is quiz closer / score submission
-// Final step is scoreboard viewing
-
-// Step 1 can be broken down into further pieces:
-// The user is presented with a screen that has a 'start quiz' button
-// When the user clicks the start button, hidden elements become visible and are filled in with a quiz question and corresponding answer buttons
-// Each question needs to have an associated data set for the answer button options
-// Each question needs at least 1 of the buttons to be true and the remainder to return false values when clicked
-// A message is displayed on the bottom if their choice was right or wrong
-// A new question is selected
-
 // ----------- VARIABLES ----------- 
 
 const quizStart = document.querySelector("#quizstart");
